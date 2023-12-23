e26f4ad0c4f70bb9bc1cc2f8a4d85525cfde5533
zhangll01 <zhangll01@sccba.org>
2023-12-24 01:36:58 +0800

    modify

--- convert.py
+++ convert.py
@@ -3,22 +3,61 @@ import re
 import shutil
 import sys
 
-def add_space_between_english_and_chinese(text):
-    # 匹配英文单词、数字、或连续的英文标识与中文之间的位置，但不包括在方括号中的内容
-    pattern = re.compile(r'(?<=[a-zA-Z0-9\-\_\/\]\)])\s*(?=[\u4e00-\u9fa5])|(?<=[\u4e00-\u9fa5])\s*(?=[a-zA-Z0-9\-\_\/\[])')
+def process_markdown(text):
+    # 用于匹配代码块的正则表达式
+    code_block_pattern = re.compile(r'```.*?```', re.DOTALL)
+    # 用于匹配最外层中括号内容的正则表达式
+    bracket_content_pattern = re.compile(r'\[(?:[^\[\]]+|\[(?:[^\[\]]+|\[.*?\])*\])*\]')
 
-    # 忽略代码块和方括号中的内容
-    in_code_block = False
-    lines = text.split('\n')
-    for i in range(len(lines)):
-        line = lines[i]
-        if '```' in line:
-            in_code_block = not in_code_block
-        if in_code_block:
-            continue
-        lines[i] = pattern.sub(' ', line)
+    # 保存处理后的文本块
+    processed_blocks = []
+    # 上次搜索结束的位置
+    last_end = 0
 
-    return '\n'.join(lines)
+    # 找到所有代码块
+    for match in code_block_pattern.finditer(text):
+        # 获取代码块前的文本
+        block_before_code = text[last_end:match.start()]
+        # 处理代码块前的文本
+        processed_block = process_non_code_block(block_before_code, bracket_content_pattern)
+        # 添加处理后的文本到结果列表
+        processed_blocks.append(processed_block)
+        # 添加代码块到结果列表（代码块不做处理）
+        processed_blocks.append(match.group())
+        # 更新上一次搜索结束的位置
+        last_end = match.end()
+
+    # 处理最后一个代码块后的文本
+    remaining_text = text[last_end:]
+    processed_block = process_non_code_block(remaining_text, bracket_content_pattern)
+    processed_blocks.append(processed_block)
+
+    # 将所有处理后的文本块合并
+    return ''.join(processed_blocks)
+
+def process_non_code_block(text, bracket_content_pattern):
+    # 处理中括号内的内容，暂时替换为占位符
+    placeholders = []
+    def replace_with_placeholder(match):
+        placeholders.append(match.group())
+        return f'PLACEHOLDER_{len(placeholders) - 1}'
+
+    text = bracket_content_pattern.sub(replace_with_placeholder, text)
+
+    # 在英文单词/数字与中文之间添加空格
+    text = re.sub(r'([a-zA-Z0-9])([\u4e00-\u9fff])', r'\1 \2', text)
+    text = re.sub(r'([\u4e00-\u9fff])([a-zA-Z0-9])', r'\1 \2', text)
+
+    # 还原中括号内的内容
+    for i, placeholder in enumerate(placeholders):
+        text = text.replace(f'PLACEHOLDER_{i}', placeholder)
+
+    return text
+
+def backup_file(file_path):
+    # 备份原始文件
+    backup_path = file_path + '.bak'
+    shutil.copyfile(file_path, backup_path)
 
 def process_file(file_path):
     # 读取原始文件内容
@@ -26,15 +65,14 @@ def process_file(file_path):
         original_content = file.read()
 
     # 添加空格
-    modified_content = add_space_between_english_and_chinese(original_content)
+    modified_content = process_markdown(original_content)
 
     # 检查是否需要备份
     with open(file_path, 'r', encoding='utf-8') as file:
         existing_content = file.read()
     if existing_content != modified_content:
         # 备份原始文件
-        backup_path = file_path + '.bak'
-        shutil.copyfile(file_path, backup_path)
+        backup_file(file_path)
 
         # 将修改后的内容写回文件
         with open(file_path, 'w', encoding='utf-8') as file:
